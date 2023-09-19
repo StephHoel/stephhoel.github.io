770863eb8846c2fd9c88f7aa352c5341c796818a
Steph Hoel <steph.hoel@gmail.com>
2023-09-19 13:58:30 -0300

    feat: add docs

---
+++ docs/script.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle('light')
+
+  // const img = document.querySelector('#profile img')
+
+  // if (html.classList.contains('light')) {
+  //   img.setAttribute('src', './assets/avatar-light.png')
+  //   img.setAttribute('alt','Foto de Mayk Brito usando óculos escuros, camisa preta, jaqueta preta, sem barba no fundo gradiente do lilás para o azul')
+  // } else {
+  //   img.setAttribute('src', './assets/avatar.png')
+  //   img.setAttribute('alt','Foto de Mayk Brito usando óculos, camisa preta, barba e fundo laranja')
+  // }
+}
